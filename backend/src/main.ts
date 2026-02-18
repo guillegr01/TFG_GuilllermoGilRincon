d5e0296063848903b313f57b40422b874d8f1a0a
guillegr01 <gllrmglrncn2001@gmail.com>
2026-02-18 19:41:34 +0100

    creating and testing the server

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,19 @@
-console.log("Hola Mundo");
+import express from 'express'
+
+//Creating server
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+//ROUTES
+
+
+//endpoint prueba
+app.get('/ping', (req, res) => {
+    res.json("Server running on LocalHost...");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}.`);
+});
