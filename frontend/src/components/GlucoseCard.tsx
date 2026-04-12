9b268dca337f195f36305b8609b8d5d0815501d1
guillegr01 <gllrmglrncn2001@gmail.com>
2026-04-12 19:26:32 +0200

    creating glucose view on dashboard

---
+++ frontend/src/components/GlucoseCard.tsx
@@ -0,0 +1,57 @@
+import { View, Text, StyleSheet } from "react-native";
+
+type Props = {
+    value: number;
+    low: number;
+    inRange: number;
+    high: number;
+    veryHigh: number;
+};
+
+export default function GlucoseCard({ value, low, inRange, high, veryHigh }: Props) {
+    const getColor = () => {
+        if (value < low) return "#ef4444"; 
+        if (value <= inRange) return "#10b981"; 
+        if (value <= high) return "#f1e425e3";
+        if (value <= veryHigh) return "#fb923c";
+        return "#fc9a2a"; 
+    };
+
+    return (
+        <View style={[styles.card, { backgroundColor: getColor() }]}>
+        <Text style={styles.label}>Last Glucose Value</Text>
+
+        <Text style={[styles.value, { color: "white" }]}>
+            {value}
+        </Text>
+
+        <Text style={styles.unit}>mg/dL</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        borderWidth: 2,
+        borderRadius: 16,
+        padding: 20,
+        alignItems: "center",
+        marginBottom: 20,
+        shadowColor: "#000",
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        elevation: 3,
+    },
+    label: {
+        fontSize: 16,
+        color: "#ffffff",
+    },
+    value: {
+        fontSize: 48,
+        fontWeight: "bold",
+    },
+    unit: {
+        fontSize: 14,
+        color: "#ffffff",
+    },
+});
--- frontend/src/tabs/dashboard/DashboardTab.tsx
+++ frontend/src/tabs/dashboard/DashboardTab.tsx
@@ -1,7 +1,12 @@
 import { useDashboard } from "@/src/hooks/useDashBoard";
-import { View, Text, ActivityIndicator } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import GlucoseCard from "../../components/GlucoseCard";
 
 
+/**
+ * * DashboardTab
+ * @returns 
+ */
 export default function DashboardTab() {
 
     const { data, loading } = useDashboard("699c9b32d5e6e90cb3b09cf1");
@@ -14,22 +19,48 @@ export default function DashboardTab() {
         return <Text>Error loading data</Text>;
     }
 
+    //saving main data from the api
+    const glucose = data.glucoseRegisters?.[0]?.glucoseValue;
+    const meal = data.meals?.[0];
+    const limits = data.therapy.glucoseLimits;
+
     return (
-        <View style={{ padding: 20 }}>
-            <Text>Dashboard</Text>
-            
-            <Text>
-                Glucosa actual:{" "}
-                {data.glucoseRegisters?.[0]?.glucoseValue || "No disponible"}
-            </Text>
+        <View style={styles.container}>
+            {glucose !== undefined && (
+                <GlucoseCard
+                value={glucose}
+                low={limits.lowLimit}
+                inRange={limits.inRangeLimit}
+                high={limits.highLimit}
+                veryHigh={limits.veryHighLimit}
+                />
+            )}
 
-            <Text>
-                Último bolus: {data.meals?.[0]?.totalBolus || "No disponible"}
-            </Text>
-
-            <Text>
-                Carbohidratos: {data.meals?.[0]?.grams || "No disponible"}
-            </Text>
+            <View style={styles.infoCard}>
+                <Text style={styles.infoTitle}>Last Meal</Text>
+                <Text>Carbs: {meal?.grams ?? "-"}</Text>
+                <Text>Bolus: {meal?.totalBolus ?? "-"}</Text>
+                <Text>Period: {meal?.period ?? "-"}</Text>
+            </View>
         </View>
     );
 }
+
+//styles for DashBoard tab
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#F9FAFB",
+    },
+    infoCard: {
+        backgroundColor: "white",
+        padding: 16,
+        borderRadius: 12,
+        elevation: 2,
+    },
+    infoTitle: {
+        fontWeight: "bold",
+        marginBottom: 10,
+    },
+});
