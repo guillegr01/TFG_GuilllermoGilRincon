e0291a0940e70dcc5682e9a7f67f67f848689b0b
guillegr01 <gllrmglrncn2001@gmail.com>
2026-06-02 19:28:03 +0200

    testing entire converter functions

--- backend/tests/converters.test.ts
+++ backend/tests/converters.test.ts
@@ -1,13 +1,17 @@
 import { describe, test, expect } from '@jest/globals';
-import { fromModelToUser } from '../src/utils/converters'; 
-import { UserModel } from '../src/types/ddbbModel';
+import { fromModelToCarbohydrateIntake, fromModelToGlucoseRegister, fromModelToTherapy, fromModelToUser } from '../src/utils/converters'; 
+import { CarbohydrateIntakeModel, GlucoseRegisterModel, TherapyModel, UserModel } from '../src/types/ddbbModel';
 import { ObjectId } from 'mongodb';
-import { User } from '../src/types/types';
+import { CarbohydrateIntake, GlucoseLimits, GlucoseRegister, InsulinRatio, Therapy, User } from '../src/types/types';
 
 
 describe('Unit test: testing converters functions', () => {
 
     test('Testing User converter function', () => {
+
+        //Arrange
+        const birthDate_var = new Date();
+        const registerDate_var = new Date();
 
         const UserModelTest: UserModel = {
             _id: new ObjectId,
@@ -15,9 +19,9 @@ describe('Unit test: testing converters functions', () => {
             surname: "García",
             email: "manuel@gmail.com",
             password: "1234",
-            birthDate: new Date(),
+            birthDate: birthDate_var,
             diabetesType: "tipo1",
-            registerDate: new Date()
+            registerDate: registerDate_var
         }
 
         const UserTest: User = {
@@ -25,14 +29,117 @@ describe('Unit test: testing converters functions', () => {
             name: "Manuel",
             surname: "García",
             email: "manuel@gmail.com",
-            birthDate: new Date(),
+            birthDate: birthDate_var,
             diabetesType: "tipo1",
-            registerDate: new Date()
+            registerDate: registerDate_var
         }
 
+        //Act
         const result = fromModelToUser(UserModelTest);
 
+        //Assert
         expect(result).toStrictEqual(UserTest);
+
+    });
+
+    test('Testing GlucoseRegister converter function', () => {
+
+        //Arrange
+        const date_hour_var = new Date();
+
+        const GlucoseRegisterModelTest: GlucoseRegisterModel = {
+            _id: new ObjectId,
+            userId: "1",
+            glucoseValue: 120,
+            date_hour: date_hour_var,
+            registerMethod: "manual"
+        }
+
+        const GlucoseRegisterTest: GlucoseRegister = {
+            id: GlucoseRegisterModelTest._id!.toString(),
+            userId: "1",
+            glucoseValue: 120,
+            date_hour: date_hour_var,
+            registerMethod: "manual"
+        }
+
+        //Act
+        const result = fromModelToGlucoseRegister(GlucoseRegisterModelTest);
+
+        //Assert
+        expect(result).toStrictEqual(GlucoseRegisterTest);
+
+    });
+
+    test('Testing Meals converter function', () => {
+
+        //Arrange
+        const date_hour_var = new Date();
+
+        const MealModelTest: CarbohydrateIntakeModel = {
+            _id: new ObjectId,
+            userId: "1",
+            grams: 40,
+            glucoseValue: 120,
+            period: "desayuno",
+            date_hour: date_hour_var,
+            totalBolus: 9.5
+        }
+
+        const MealsTest: CarbohydrateIntake = {
+            id: MealModelTest._id!.toString(),
+            userId: "1",
+            grams: 40,
+            glucoseValue: 120,
+            period: "desayuno",
+            date_hour: date_hour_var,
+            description: undefined,
+            foodImages: undefined,
+            totalBolus: 9.5
+        }
+
+        //Act
+        const result = fromModelToCarbohydrateIntake(MealModelTest);
+
+        //Assert
+        expect(result).toStrictEqual(MealsTest);
+
+    });
+
+    test('Testing therapy converter function', () => {
+
+        //Arrange
+        const ratios: InsulinRatio[] = [
+            {period:"desayuno", ratio:1.5, sensibilityFactor:50},
+            {period:"comida", ratio:1, sensibilityFactor:50},
+            {period:"cena", ratio:1.5, sensibilityFactor:50},
+        ];
+
+        const limits: GlucoseLimits = {lowLimit:70, inRangeLimit:180, highLimit:250, veryHighLimit:400};
+
+        const TherapyModelTest: TherapyModel = {
+            _id: new ObjectId,
+            userId: "1",
+            ratios: ratios,
+            glucoseTarget: 110,
+            insulinActive: 3,
+            glucoseLimits: limits
+        }
+
+        const TherapyTest: Therapy = {
+            id: TherapyModelTest._id!.toString(),
+            userId:"1",
+            ratios: ratios,
+            glucoseTarget: 110,
+            insulinActive: 3,
+            glucoseLimits: limits
+        }
+
+        //Act
+        const result = fromModelToTherapy(TherapyModelTest);
+
+        //Assert
+        expect(result).toStrictEqual(TherapyTest);
 
     });
 
